369d0595fe869603706bccfd3b05a3b617271af6
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-04-28 09:48:40 +0200

    register doctors info cpp script

---
+++ register_doctor_info[1].cpp
@@ -0,0 +1,17 @@
+#include "hospital_information_gather.h"
+#include <iostream>
+#include <string>
+using namespace std;
+void writeColumnsNames{
+	ofstream file_object;
+	file_object.open("doctor_info.txt");
+	file_object << "Id;Name;Surname;Type_of_Doctor;Wards";
+}
+void registerDoctor(string id, string name, string surname, string type_of_doctor, string wards){
+	string line;
+	line = id+";"+name+";"+surname+";"+type_of_doctor+";"+wards;
+	ofstream file_object;
+	file_object.open("doctor_info.txt", ios:app);
+	file_object << line;
+	file_object.close();
+}
