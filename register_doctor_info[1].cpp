091ceb67cf59065129867f74df03c5c62e51a2b7
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-05-04 10:48:46 +0200

    register doctor information script

--- register_doctor_info[1].cpp
+++ register_doctor_info[1].cpp
@@ -8,6 +8,7 @@ void writeColumnsNames{
 	file_object << "Id;Name;Surname;Type_of_Doctor;Wards";
 }
 void registerDoctor(string id, string name, string surname, string type_of_doctor, string wards){
+	//file that stores doctors details on file
 	string line;
 	line = id+";"+name+";"+surname+";"+type_of_doctor+";"+wards;
 	ofstream file_object;
