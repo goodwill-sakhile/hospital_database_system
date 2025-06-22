db8a36720d75d1364d7eb23a4b9cec6d73638c65
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-06-23 01:27:12 +0200

    Main Script

---
+++ cli/Main.java
@@ -0,0 +1,11 @@
+package com.hms.cli;
+
+/**
+ * Main class to launch the CLI application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        CLIApplication app = new CLIApplication();
+        app.start();
+    }
+}
