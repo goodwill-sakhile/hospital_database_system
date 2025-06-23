37f30d039d4c376caf02e0d866af53326154d722
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-06-23 02:47:25 +0200

    Audit Action Script

---
+++ audit/AuditAction.java
@@ -0,0 +1,16 @@
+package com.hms.audit;
+
+/**
+ * Enum of standard actions.
+ */
+public enum AuditAction {
+    CREATE_PATIENT,
+    DELETE_PATIENT,
+    UPDATE_PATIENT,
+    VIEW_PATIENT,
+    LOGIN,
+    LOGOUT,
+    REGISTER_USER,
+    SYSTEM_START,
+    SYSTEM_SHUTDOWN
+}
