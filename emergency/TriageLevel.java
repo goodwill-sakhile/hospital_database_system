d10183ca3d2ea65975eb87ff9d716f18fd0b042f
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-06-23 03:42:53 +0200

    Triage Level Script

---
+++ emergency/TriageLevel.java
@@ -0,0 +1,9 @@
+package com.hms.emergency;
+
+public enum TriageLevel {
+    RED,    // Immediate
+    ORANGE, // Very Urgent
+    YELLOW, // Urgent
+    GREEN,  // Less Urgent
+    BLUE    // Non-Urgent
+}
